bf69cdbe934a566c4b951ec5737d0c80c5155baf
sh10f <13907208+f_shihao@user.noreply.gitee.com>
2024-04-21 19:44:24 +0800

    reserve

---
+++ src/View/ff.java
@@ -0,0 +1,32 @@
+/*
+ * Created by JFormDesigner on Sun Apr 14 18:04:02 CST 2024
+ */
+
+package View;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author Lenovo
+ */
+public class ff extends JFrame {
+    public ff() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+
+        //======== this ========
+        var contentPane = getContentPane();
+        new JTextField().transferFocus();
+        contentPane.setLayout(new CardLayout());
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
--- src/test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        String s = "111";
-        int a = Integer.getInteger(s);
-        System.out.println(a);
-    }
-}
